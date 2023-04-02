4b31bc1566c0ec61fc011109404c878497e52fe8
WojtekP <wojtekpietraszuk@gmail.com>
2023-04-02 16:50:22 +0200

    Create auth context and init supabase client
    The auth context has been created to manage authentication within the application. The Supabase client has been initialized. This allows the application to interact with the Supabase service

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../src/styles/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/RegisterPage";
+import Login from "./pages/LoginPage";
+import AuthContextProvider from "./context/AuthContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
   </React.StrictMode>,
 );
