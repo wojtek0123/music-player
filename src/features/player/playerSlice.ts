3987cf5cb45daa4af51794cbf0f28a1a9b2702cb
Jerzy Marczewski <jerzy.marczewski2@gmail.com>
2023-04-07 11:32:30 +0200

    added a changeSong Thunk

--- src/features/player/playerSlice.ts
+++ src/features/player/playerSlice.ts
@@ -1,18 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Song } from "../../helpers/types";
+import { supabase } from "../../helpers/supabase";
+
+const initialSong: Song = {
+  title: undefined,
+  author: undefined,
+  link: undefined,
+};
 
 export interface PlayerState {
   currentSong?: Song;
   isPlaying: boolean;
   queue: Array<Song>;
   history: Array<Song>;
+  status: "idle" | "loading" | "failed" | "succeeded";
 }
 
 const initialState: PlayerState = {
-  currentSong: undefined,
+  currentSong: initialSong,
   isPlaying: false,
   queue: [],
   history: [],
+  status: "idle",
 };
 
 export const playerSlice = createSlice({
@@ -23,6 +32,26 @@ export const playerSlice = createSlice({
       state.isPlaying = !state.isPlaying;
     },
   },
+  extraReducers(builder) {
+    builder.addCase(changeSong.fulfilled, (state, actions) => {
+      state.status = "succeeded";
+      const [song] = actions.payload;
+      state.currentSong = song;
+    });
+    builder.addCase(changeSong.pending, (state) => {
+      state.status = "loading";
+      console.log("loading");
+    });
+    builder.addCase(changeSong.rejected, (state) => {
+      state.status = "failed";
+    });
+  },
+});
+
+export const changeSong = createAsyncThunk("player/changeSong", async (id: string) => {
+  const { data: song } = await supabase.from("song").select().eq("id", id);
+
+  return song as Song;
 });
 
 // Action creators are generated for each case reducer function
