b8724b369c0b2154382ff6a88232f1a2d14640f4
Jerzy Marczewski <jerzy.marczewski2@gmail.com>
2023-04-07 12:50:25 +0200

    added mobileFullScreen state and added classes to control buttons

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -4,13 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { changeSong, playToggle } from "../features/player/playerSlice";
 import styles from "../styles/Player.module.css";
 import { useEffect, useState } from "react";
-// import { useState } from "react";
 
 export const Player = (): JSX.Element => {
   const currentSong = useSelector((state: RootState) => state.player.currentSong);
   const isPlaying = useSelector((state: RootState) => state.player.isPlaying);
   const dispatch = useDispatch<AppDispatch>();
 
+  // ! temp loading song
   useEffect(() => {
     dispatch(changeSong("ba898259-9001-44b1-a2ef-051922bcfcb3"));
   }, []);
@@ -19,8 +19,7 @@ export const Player = (): JSX.Element => {
     setMyAudio(new Audio(currentSong?.link));
   }, [currentSong]);
 
-  // const [playerIsInFullscreenView, setPlayerIsInFullscreenView] = useState<boolean>(false);
-
+  const [mobileFullscreenView, setMobileFullscreenView] = useState(false);
   const [myAudio, setMyAudio] = useState(new Audio(currentSong?.link));
 
   const albumCover = <Icon icon="akar-icons:music-album-fill" color="white" width={50} />;
@@ -39,22 +38,24 @@ export const Player = (): JSX.Element => {
     </div>
   );
 
-  const progressBar = <input type="range" name="progressBar" />;
+  const progressBar = <input className={styles.progressBar} type="range" name="progressBar" />;
 
   const repeatButton = (
-    <button>
-      <Icon icon="ph:repeat-bold" color="white" width={50} tabIndex={0} />
+    <button className={styles.repeatButton} tabIndex={0}>
+      <Icon icon="ph:repeat-bold" color="white" width={50} />
     </button>
   );
 
   const skipBackButton = (
-    <button>
-      <Icon icon="ph:skip-back-fill" color="white" width={50} tabIndex={0} />
+    <button className={styles.skipBackButton} tabIndex={0}>
+      <Icon icon="ph:skip-back-fill" color="white" width={50} />
     </button>
   );
 
   const playToggleButton = (
     <button
+      className={styles.playToggleButton}
+      tabIndex={0}
       onClick={(e) => {
         e.stopPropagation();
         dispatch(playToggle());
@@ -66,20 +67,19 @@ export const Player = (): JSX.Element => {
         icon={isPlaying ? "material-symbols:pause-circle-rounded" : "material-symbols:play-circle-rounded"}
         color="white"
         width={50}
-        tabIndex={0}
       />
     </button>
   );
 
   const skipForwardButton = (
-    <button>
-      <Icon icon="ph:skip-forward-fill" color="white" width={50} tabIndex={0} />
+    <button className={styles.skipForwardButton} tabIndex={0}>
+      <Icon icon="ph:skip-forward-fill" color="white" width={50} />
     </button>
   );
 
   const shuffleButton = (
-    <button>
-      <Icon icon="ph:shuffle-bold" color="white" width={50} tabIndex={0} />
+    <button className={styles.shuffleButton} tabIndex={0}>
+      <Icon icon="ph:shuffle-bold" color="white" width={50} />
     </button>
   );
 
