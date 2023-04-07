7887ba908b6776f4558a140467f7eb9c8f05cb6c
Jerzy Marczewski <jerzy.marczewski2@gmail.com>
2023-04-07 11:30:40 +0200

    added Buttons with no logic

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,41 +1,107 @@
 import { Icon } from "@iconify/react";
-import type { RootState } from "../app/store";
+import type { AppDispatch, RootState } from "../app/store";
 import { useDispatch, useSelector } from "react-redux";
-import { playToggle } from "../features/player/playerSlice";
-import { Song } from "../helpers/types";
+import { changeSong, playToggle } from "../features/player/playerSlice";
 import styles from "../styles/Player.module.css";
+import { useEffect, useState } from "react";
+// import { useState } from "react";
 
-type PlayerProps = {
-  song?: Song;
-};
-
-export const Player = ({ song }: PlayerProps): JSX.Element => {
+export const Player = (): JSX.Element => {
+  const currentSong = useSelector((state: RootState) => state.player.currentSong);
   const isPlaying = useSelector((state: RootState) => state.player.isPlaying);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
-  // const myAudio = new Audio(song?.link);
+  useEffect(() => {
+    dispatch(changeSong("ba898259-9001-44b1-a2ef-051922bcfcb3"));
+  }, []);
+
+  useEffect(() => {
+    setMyAudio(new Audio(currentSong?.link));
+  }, [currentSong]);
+
+  // const [playerIsInFullscreenView, setPlayerIsInFullscreenView] = useState<boolean>(false);
+
+  const [myAudio, setMyAudio] = useState(new Audio(currentSong?.link));
 
   const albumCover = <Icon icon="akar-icons:music-album-fill" color="white" width={50} />;
 
-  const playToggleButton = (
-    <Icon
-      icon={isPlaying ? "material-symbols:pause-circle-rounded" : "material-symbols:play-circle-rounded"}
-      color="white"
-      width={50}
-      onClick={() => dispatch(playToggle())}
-    />
+  const songBox = (
+    <div className={styles.songBox}>
+      {albumCover}
+      <div className={styles.songInfo}>
+        <button className={styles.title} tabIndex={0}>
+          {currentSong?.title ?? "unknown"}
+        </button>
+        <button className={styles.author} tabIndex={0}>
+          {currentSong?.author ?? "unknown"}
+        </button>
+      </div>
+    </div>
   );
+
+  const progressBar = <input type="range" name="progressBar" />;
+
+  const repeatButton = (
+    <button>
+      <Icon icon="ph:repeat-bold" color="white" width={50} tabIndex={0} />
+    </button>
+  );
+
+  const skipBackButton = (
+    <button>
+      <Icon icon="ph:skip-back-fill" color="white" width={50} tabIndex={0} />
+    </button>
+  );
+
+  const playToggleButton = (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        dispatch(playToggle());
+        console.log(myAudio.duration);
+        isPlaying ? myAudio?.pause() : myAudio?.play();
+      }}
+    >
+      <Icon
+        icon={isPlaying ? "material-symbols:pause-circle-rounded" : "material-symbols:play-circle-rounded"}
+        color="white"
+        width={50}
+        tabIndex={0}
+      />
+    </button>
+  );
+
+  const skipForwardButton = (
+    <button>
+      <Icon icon="ph:skip-forward-fill" color="white" width={50} tabIndex={0} />
+    </button>
+  );
+
+  const shuffleButton = (
+    <button>
+      <Icon icon="ph:shuffle-bold" color="white" width={50} tabIndex={0} />
+    </button>
+  );
+
+  const controls = (
+    <div className={styles.controls}>
+      {progressBar}
+      <div className={styles.controlButtons}>
+        {repeatButton}
+        {skipBackButton}
+        {playToggleButton}
+        {skipForwardButton}
+        {shuffleButton}
+      </div>
+    </div>
+  );
+
+  if (currentSong?.link === undefined) return <></>;
 
   return (
     <div className={styles.container}>
-      <div className={styles.songBox}>
-        {albumCover}
-        <div className={styles.songInfo}>
-          <div className={styles.title}>{song?.title ?? "unknown"}</div>
-          <div className={styles.author}>{song?.author ?? "unknown"}</div>
-        </div>
-      </div>
-      {playToggleButton}
+      {songBox}
+      {controls}
     </div>
   );
 };
