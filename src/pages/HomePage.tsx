6d6ab67295f1c4392575657ecce1f37d85457a1b
WojtekP <wojtekpietraszuk@gmail.com>
2023-04-18 19:19:13 +0200

    Implement refetch session on start app
    This ensures that the user's session remains active even if the app is closed in between usage. With this new feature, the user will not have to go through the login process every time they reopen the app

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,26 @@
+import { useEffect } from "react";
 import Navigation from "../components/Navigation";
 import styles from "../styles/Home.module.css";
-// import { Song } from "../helpers/types";
-// import { Player } from "../components/Player";
-
-// ! only temp - remove later
-// const song: Song = {
-//   title: "song",
-//   author: "author",
-//   length: 124,
-//   link: "https://ptjdojdwwccirjiuvuut.supabase.co/storage/v1/object/sign/songs/lofi-chill-140858.mp3?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzb25ncy9sb2ZpLWNoaWxsLTE0MDg1OC5tcDMiLCJpYXQiOjE2ODA1MTUyNDAsImV4cCI6MTcxMjA1MTI0MH0.JAeXb0lIvDWisIhaVJmtAN6qUyj-oN5WhVC8iyFrDyA&t=2023-04-03T09%3A47%3A08.395Z",
-// };
+import { useDispatch } from "react-redux";
+import { supabase } from "../helpers/supabase";
+import { setSession } from "../features/auth/authSlice";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      dispatch(setSession(session));
+    })();
+  }, [dispatch]);
+
   return (
     <div className={styles.container}>
-      <Navigation></Navigation>
-      {/* <Player song={song} /> */}
+      <Navigation />
     </div>
   );
 };
