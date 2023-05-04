8565cc4b3facb2397d6c7961f2827ba387021de3
WojtekP <wojtekpietraszuk@gmail.com>
2023-05-04 18:51:57 +0200

    Add size props and improve ui
    Song component has wide and standard size. Add context menu and play song button

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -5,12 +5,11 @@ import styles from "../styles/Song.module.css";
 interface SongProps {
   song: Song;
   size: "wide" | "standard";
+  details: boolean;
 }
 
-const Song = ({ song, size }: SongProps) => {
+const Song = ({ song, size, details = false }: SongProps) => {
   const getSongTime = () => {
-    // const songsLengthInSeconds = playlist?.songs.reduce((prev, curr) => prev + curr.time, 0) ?? 0;
-
     const hours = Math.floor(song.time / 3600);
     const minutes = Math.floor((song.time % 3600) / 60);
     const seconds = Math.floor(song.time % 60);
@@ -23,16 +22,22 @@ const Song = ({ song, size }: SongProps) => {
   return (
     <div className={size === "wide" ? styles.wide + " " + styles.song : styles.standard + " " + styles.song}>
       <Icon className={styles.icon} icon="zondicons:music-album" color="white" width="100%" />
-      <div className={styles.wrapper}>
+      <div className={details ? styles["wrapper-with-details"] : styles.wrapper}>
         <div className={styles.title}>
           <h3>{song.title}</h3>
           <p>{song?.author?.name ?? ""}</p>
         </div>
-        <div className={styles.details}>
+        <div className={details ? styles.details : styles.hide}>
           <span>{getSongTime()}</span>
         </div>
-        <button type="button" className={styles["like-btn"]}>
+        <button type="button" aria-label="Like" className={styles["like-btn"]}>
           <Icon icon="mdi:cards-heart-outline" color="white" />
+        </button>
+        <button type="button" aria-label="Play" className={styles["play-btn"]}>
+          <Icon icon="material-symbols:play-circle-rounded" width="100%" color="white" />
+        </button>
+        <button type="button" aria-label="Context menu" className={styles["context-menu-btn"]}>
+          <Icon icon="mdi:dots-vertical" color="white" width="100%" />
         </button>
       </div>
     </div>
