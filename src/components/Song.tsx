0deae7285529a6c3498496c3a758276116b52489
WojtekP <wojtekpietraszuk@gmail.com>
2023-06-20 08:27:23 +0200

    Move part of the logic to redux slice
    Do this to improve reusability of the reducers from redux

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -69,7 +69,6 @@ const Song = ({ song, size, details, playlistOwnerId, playlistId }: SongProps) =
   };
 
   const addToPlaylist = async (playlistId: string) => {
-    // todo: after adding a song to playlist pop up should disappear
     const filteredPlaylist = userPlaylists.find((playlist) => playlist.id === playlistId);
 
     dispatch(hideMenu());
@@ -94,35 +93,15 @@ const Song = ({ song, size, details, playlistOwnerId, playlistId }: SongProps) =
   };
 
   const addToLikedSongs = async () => {
-    const { error } = await supabase
-      .from("songs")
-      .insert({ song_id: song.id, playlist_id: likedSongsPlaylist?.id ?? "" });
-    if (error) {
-      toast.error(error.message, options);
-      return;
-    }
-    dispatch(addToLikedSongsPlaylist(song));
-    toast.success("Successfully added song to liked songs", options);
+    dispatch(addToLikedSongsPlaylist({ song, likedSongsPlaylistId: likedSongsPlaylist?.id ?? "" }));
   };
 
   const removeFromLikedSongs = async () => {
-    const { error } = await supabase
-      .from("songs")
-      .delete()
-      .match({ song_id: song.id, playlist_id: likedSongsPlaylist?.id ?? "" });
-
-    if (error) {
-      toast.error(error.message, options);
-      return;
-    }
-
-    dispatch(removeFromLikedSongsPlaylist(song.id));
+    dispatch(removeFromLikedSongsPlaylist({ songId: song.id, likedSongsPlaylistId: likedSongsPlaylist?.id ?? "" }));
 
     if (playlistId === likedSongsPlaylist?.id) {
       dispatch(removeSongFromSelectedPlaylist(song.id));
     }
-
-    toast.success("Successfully removed song from playlist", options);
   };
 
   const isSongIncluded = () => {
